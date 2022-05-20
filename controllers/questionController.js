c4ceb56c6e95700d7e1f0701f2122010077aae83
Alberto <tejotaesi@gmail.com>
2022-05-20 20:14:46 -0300

    This commit changes the name of the collections to be fetched

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -2,7 +2,7 @@ import db from "../db.js";
 
 export async function getQuestionsJavascript(req,res){
   try {
-    const questions = await db.collection("Javascript").find({}).toArray();
+    const questions = await db.collection("JavaScript").find({}).toArray();
     res.send(questions);
   } catch (err){
       console.log(err);
@@ -22,7 +22,7 @@ export async function getQuestionsReact(req,res){
 
 export async function getQuestionsMongo(req,res){
   try {
-    const questions = await db.collection("Mongo").find({}).toArray();
+    const questions = await db.collection("MongoDB").find({}).toArray();
     res.send(questions);
   } catch (err){
       console.log(err);
