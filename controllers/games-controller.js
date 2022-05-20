427185d3be0d78ab78d0b72420f267078aa0f01b
Felipe <felipecosta1961@gmail.com>
2022-05-20 19:09:04 -0300

    Get games feat done.

--- controllers/games-controller.js
+++ controllers/games-controller.js
@@ -7,7 +7,6 @@ export async function getGames(req, res) {
     if (games.length > 0) return res.send(games);
 
     res.status(404).send("Não existe nenhuma opção até o momento");
-    console.log("não achei");
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
