285f15f117d8a5c3df7bb2ae738bcff632d8a5b7
Felipe <felipecosta1961@gmail.com>
2022-05-20 20:20:23 -0300

    Question Controller changes

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,31 +1,31 @@
 import db from "../db.js";
 
-export async function getQuestionsJavascript(req,res){
+export async function getQuestionsJavascript(req, res) {
   try {
     const questions = await db.collection("Javascript").find({}).toArray();
     res.send(questions);
-  } catch (err){
-      console.log(err);
-      res.sendStatus(500);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
   }
 }
 
-export async function getQuestionsReact(req,res){
+export async function getQuestionsReact(req, res) {
   try {
     const questions = await db.collection("React").find({}).toArray();
     res.send(questions);
-  } catch (err){
-      console.log(err);
-      res.sendStatus(500);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
   }
 }
 
-export async function getQuestionsMongo(req,res){
+export async function getQuestionsMongo(req, res) {
   try {
     const questions = await db.collection("Mongo").find({}).toArray();
     res.send(questions);
-  } catch (err){
-      console.log(err);
-      res.sendStatus(500);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
   }
 }
