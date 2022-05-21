acf82e752bb1ed36cb7247ee6943cfc9caee10d7
Felipe <felipecosta1961@gmail.com>
2022-05-20 21:28:57 -0300

    This commit fixes question router export.

--- routes/questionRouter.js
+++ routes/questionRouter.js
@@ -3,10 +3,10 @@ import { getQuestionsJavascript } from "../controllers/questionController.js";
 import { getQuestionsReact } from "../controllers/questionController.js";
 import { getQuestionsMongo } from "../controllers/questionController.js";
 
-const productsRouter = Router();
+const questionRouter = Router();
 
 questionRouter.get("/javascript", getQuestionsJavascript);
 questionRouter.get("/react", getQuestionsReact);
 questionRouter.get("/mongodb", getQuestionsMongo);
 
-export default productsRouter;
+export default questionRouter;
