e9d37e4134a8c25f17b5828892ec591c10c09807
Felipe <felipecosta1961@gmail.com>
2022-05-20 21:25:27 -0300

    Solving conflicts on question router.

--- routes/questionRouter.js
+++ routes/questionRouter.js
@@ -5,14 +5,8 @@ import { getQuestionsMongo } from "../controllers/questionController.js";
 
 const productsRouter = Router();
 
-<<<<<<< HEAD
-productsRouter.get("/javascript", getQuestionsJavascript);
-productsRouter.get("/react", getQuestionsReact);
-productsRouter.get("/react", getQuestionsMongo);
-=======
 questionRouter.get("/javascript", getQuestionsJavascript);
 questionRouter.get("/react", getQuestionsReact);
 questionRouter.get("/mongodb", getQuestionsMongo);
->>>>>>> 5460e9f00eb868ab53e4f6a50b48f36603a9b787
 
 export default productsRouter;
